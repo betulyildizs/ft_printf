9db39b39da2e3de50aa20f5556a0b1d910b436cd
betulyildizs <120586474+betulyildizs@users.noreply.github.com>
2024-01-09 19:17:41 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: beyildiz <beyildiz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 15:37:51 by beyildiz          #+#    #+#             */
-/*   Updated: 2024/01/09 19:16:23 by beyildiz         ###   ########.fr       */
+/*   Updated: 2024/01/09 19:17:16 by beyildiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,4 @@ int		ft_printf(const char *by)
 	}
 	else
 		ft_printstr(by);
-}
-int main()
-{
-	char *by = "a";
-	ft_printf(by);
-	
 }
