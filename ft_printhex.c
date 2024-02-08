e8bad1aea5268d4e473ed1a2e89c1b3e1589abc4
betulyildizs <120586474+betulyildizs@users.noreply.github.com>
2024-02-09 00:11:15 +0300

    Update ft_printhex.c

--- ft_printhex.c
+++ ft_printhex.c
@@ -12,32 +12,32 @@
 
 #include "ft_printf.h"
 
-void	ft_puthex(unsigned int num, const char str)
+void	ft_puthex(unsigned int nbr, const char str)
 {
-	if (num >= 16)
+	if (nbr >= 16)
 	{
-		ft_puthex(num / 16, str);
-		ft_puthex(num % 16, str);
+		ft_puthex(nbr / 16, str);
+		ft_puthex(nbr % 16, str);
 	}
 	else
 	{
-		if (num <= 9)
-			ft_putchar_fd((num + '0'), 1);
+		if (nbr <= 9)
+			ft_putchar_fd((nbr + '0'), 1);
 		else
 		{
 			if (str == 'x')
-				ft_putchar_fd((num - 10 + 'a'), 1);
+				ft_putchar_fd((nbr - 10 + 'a'), 1);
 			if (str == 'X')
-				ft_putchar_fd((num - 10 + 'A'), 1);
+				ft_putchar_fd((nbr - 10 + 'A'), 1);
 		}
 	}
 }
 
-int	ft_printhex(unsigned int num, const char str)
+int	ft_printhex(unsigned int nbr, const char str)
 {
-	if (num == 0)
+	if (nbr == 0)
 		return (write(1, "0", 1));
 	else
-		ft_puthex(num, str);
-	return (ft_hexlen(num));
+		ft_puthex(nbr, str);
+	return (ft_hexlen(nbr));
 }
